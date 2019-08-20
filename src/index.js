fba7fa51bbf7c0ae89f449987deee5685c02be6a
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-08-19 21:50:18 -0400

    Used hooks
    Used hooks to make a input field that keeps track of values

--- src/index.js
+++ src/index.js
@@ -5,14 +5,31 @@ import "./styles.css";
 
 function App() {
   //The count seems like the variable, the setCount changes the variable, the useState is the default value of count
-  var [count, setCount] = useState(0);
-
+  var [fruit, setFruit] = useState("");
   //It seems like the second parameter returns everything as the value of count
+
+  var text = "";
+  var setText = event => {
+    text = event.target.value;
+  };
+
+  //When enter is pressed, set text to the value of fruit
+  var onEnterPress = a => {
+    if (a.key === "Enter") {
+      setFruit(text);
+      text = "";
+    }
+  };
+
   return (
     <div className="App">
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Hello</button>
-      <div>Blahblah</div>
+      <div>{fruit}</div>
+      <input
+        type="text"
+        placeholder="Type in the fruit"
+        onKeyPress={onEnterPress}
+        onChange={setText}
+      />
     </div>
   );
 }
